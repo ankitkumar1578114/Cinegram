39339f6fe0bb3e9e415b21fdb57b49df75131ca8
sandeepningwal5@gmail.com <sandeepningwal5@gmail.com>
2022-10-26 00:17:53 +0530

    global search feature implemented

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,44 +1,56 @@
-import { useEffect, useState } from 'react';
-import BackendUrl  from '../../urls';
+import { useEffect, useState, useRef } from 'react';
+import BackendUrl from '../../urls';
 import MovieList from '../list/movie'
 import './index.css';
 
 
-const urlToFetch =  `${BackendUrl}/movie/search/`
+const urlToFetch = `${BackendUrl}/movie/search/`
 
-const Componenet = ()=> {
+const Componenet = () => {
 
-    const [itemToSearch,setItemToSearch]= useState()
-    const [movieList,setMovieList] = useState();
+    var itemToSearch = ""
+    const [searchList, setSearchList] = useState([]);
 
-    const searchFunc = e => {
+    const searchFunc = (e) => {
         // e.preventDefault();
-        setItemToSearch(e.target.value)
-        if(itemToSearch=="")
-        return ;
-        fetch(urlToFetch+itemToSearch,{
-            method : "GET",
-            mode: 'cors',
-            headers:{} 
-        })
-        .then(res=>res.json())
-        .then(json=>{
-            // console.log(json)
-            setItemToSearch("");
-            setMovieList(json);
-        });
-    }
+        itemToSearch = e.target.value;
 
+        // console.log(itemToSearch)
+
+        if (itemToSearch == "") {
+            setSearchList([])
+            return;
+        }
+
+        fetch(urlToFetch + itemToSearch, {
+            method: "GET",
+            mode: 'cors',
+            headers: {}
+        })
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json)
+                setSearchList(json);
+            });
+    }
 
     return (
         <>
             <form onSubmit={searchFunc}>
-                <input className="search-box" placeholder="Search for movie here..." onChange = { (e) =>{searchFunc(e) }} type="text"/>
+                <input className="search-box" placeholder="Search for movie here..." onChange={(e) => { searchFunc(e) }} type="text" />
             </form>
 
-               <MovieList movies = {movieList} calledFrom ="searchPage" />
-
-            {/* <List movieList={movieList}/> */}
+            <div className="search-list" >
+                {
+                    searchList.map((movie) => (
+                        <div class='search-list-item'>
+                            {movie.title}
+                        </div>
+                    )
+                    )
+                }
+                
+            </div>
         </>
     );
 
