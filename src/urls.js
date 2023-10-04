3b781c4320d8e482a3397d3b7c282690a155a051
Ankit Kumar <ankitkumar1578114@gmail.com>
2023-10-04 11:58:03 +0530

    changes in url

--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,6 @@
 // const BackendUrl = "http://localhost:4000";
 // const BackendUrl = "http://10.1.56.192:4000";
-const BackendUrl = "http://10.1.24.164:4000";
+const BackendUrl = "https://talkie-backend.vercel.app";
 
 export default BackendUrl;
 
